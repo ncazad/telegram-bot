80d4c9ca1045092b891c7e69ab11fbd010c38a5b
Azadx69x <nezukojerry803@gmail.com>
2026-04-07 00:08:07 +0600

    tg-bot

---
+++ cmds/imgur.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+export default {
+  name: "imgur",
+  version: "1.0.0",
+  author: "Azadx69x",
+  role: 0,
+  category: "upload",
+  execute: async (client, message, args) => {
+    const chatId = message.chat.id;
+    let mediaUrl = null;
+
+    const urlArg = args.join(" ");
+    if (urlArg && /^https?:\/\//i.test(urlArg)) {
+      mediaUrl = urlArg;
+    } else if (message.reply_to_message) {
+      const reply = message.reply_to_message;
+      let fileId = null;
+      if (reply.photo) fileId = reply.photo[reply.photo.length - 1].file_id;
+      else if (reply.video) fileId = reply.video.file_id;
+      else if (reply.document) fileId = reply.document.file_id;
+      if (fileId) {
+        const fileInfo = await client.getFile(fileId);
+        mediaUrl = "https://api.telegram.org/file/bot" + client.token + "/" + fileInfo.file_path;
+      }
+    } else {
+      let fileId = null;
+      if (message.photo) fileId = message.photo[message.photo.length - 1].file_id;
+      else if (message.video) fileId = message.video.file_id;
+      else if (message.document) fileId = message.document.file_id;
+      if (fileId) {
+        const fileInfo = await client.getFile(fileId);
+        mediaUrl = "https://api.telegram.org/file/bot" + client.token + "/" + fileInfo.file_path;
+      }
+    }
+
+    if (!mediaUrl) return client.sendMessage(chatId, "❌ No media found.");
+
+    try {
+      const endpoint = "https://azadx69x.is-a.dev/api/imgur?url=" + encodeURIComponent(mediaUrl);
+      const response = await axios.get(endpoint, { timeout: 20000 });
+      const data = response.data;
+      if (!data || !data.url) return client.sendMessage(chatId, "❌ Upload failed.");
+      client.sendMessage(chatId, data.url);
+    } catch {
+      client.sendMessage(chatId, "❌ Error uploading.");
+    }
+  }
+};
